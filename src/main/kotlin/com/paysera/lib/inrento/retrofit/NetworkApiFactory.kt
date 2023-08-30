94d5203eea60bd35c0bb736026f51aa9947cbb18
Gilbert Parreño <gilbert.parreno@paysera.net>
2023-08-30 13:32:13 +0800

    revert some changes

--- src/main/kotlin/com/paysera/lib/inrento/retrofit/NetworkApiFactory.kt
+++ src/main/kotlin/com/paysera/lib/inrento/retrofit/NetworkApiFactory.kt
@@ -1,15 +1,21 @@
 package com.paysera.lib.inrento.retrofit
 
+import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.paysera.lib.common.interfaces.BaseApiCredentials
 import com.paysera.lib.common.interfaces.ErrorLoggerInterface
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
+import com.paysera.lib.common.serializers.DateSerializer
+import com.paysera.lib.common.serializers.MoneySerializer
 import com.paysera.lib.inrento.clients.InRentoApiClient
 import com.paysera.lib.inrento.entities.project.PSProject
 import com.paysera.lib.inrento.serializers.ProjectDeserializer
 import okhttp3.Interceptor
 import okhttp3.logging.HttpLoggingInterceptor
+import org.joda.money.Money
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 class NetworkApiFactory(
     baseUrl: String,
@@ -38,7 +44,14 @@ class NetworkApiFactory(
         }
     }
 
-    override var doOnCreateGsonConverterFactory: ((GsonBuilder) -> Unit)? = { gsonBuilder ->
-        gsonBuilder.registerTypeAdapter(PSProject::class.java, ProjectDeserializer())
+    override fun createGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create(
+            GsonBuilder().apply {
+                setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                registerTypeAdapter(Money::class.java, MoneySerializer())
+                registerTypeAdapter(Date::class.java, DateSerializer())
+                registerTypeAdapter(PSProject::class.java, ProjectDeserializer())
+            }.create()
+        )
     }
 }
