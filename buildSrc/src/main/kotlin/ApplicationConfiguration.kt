eda8582930e881dd7b01e9fda4f4dc50ca9e93d0
Gilbert Parreño <gilbert.parreno@paysera.net>
2023-08-31 08:28:41 +0800

    update version to 1.0.14

--- buildSrc/src/main/kotlin/ApplicationConfiguration.kt
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -1,8 +1,7 @@
 object ApplicationConfiguration {
     const val artifactId = "kotlin-lib-inrento-sdk"
     const val groupId = "com.paysera.lib"
-    // can be changed once other PRs were merged
-    const val version = "1.0.15"
+    const val version = "1.0.14"
     const val name = "Kotlin Library InRento SDK"
     const val url = "https://www.paysera.com/v2/en-GB/index"
     const val description = "Paysera: Kotlin InRento SDK Library"
